64e9e5ed0cc9291b531225dd340ed06207638653
John Hasznosi <noctiscreo@gmail.com>
2019-12-05 17:06:29 +0000

    Front end for 'bankingapp' API using React. Currently displays mvp of names and balances.

---
+++ src/DisplayAccounts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './DisplayAccounts';
+
+function displayAccounts() {
+    return (
+        <div>
+            Accounts:
+
+        </div>
+    );
+}
+
+export default displayAccounts;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function useGetAccounts() {
+    // sets up local state inside function
+    // accounts = variable that stores state
+    // setAccountsData function that updates the Accounts variable
+    const [accounts, setAccountsData] = useState([])
+    useEffect(()=>{
+        // Makes request to URL:
+        fetch('http://localhost:5000/SQbank')
+            // formats the Json
+            .then((data)=>{return data.json()})
+            // .then (above) followed by a return passes the data down to the next .then as a parameter (below)
+            .then((data)=>{
+                // data is the array of objects in Json
+                setAccountsData(data)
+            })
+    }, accounts)
+    return accounts
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Accounts() {
+    const accounts = useGetAccounts()
+    const accountElements = accounts.map((account) =>
+        <p>{account.name}: {account.balance}</p>
+    );
+
+
+    return (
+        <div className="App">
+            <h1> { accountElements }</h1>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<Accounts />, document.getElementById('root'));
+
+
