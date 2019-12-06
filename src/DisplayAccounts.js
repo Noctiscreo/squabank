4a81b69fe83c657c163e1041dce46a31d1e666e0
John Hasznosi <noctiscreo@gmail.com>
2019-12-06 11:57:19 +0000

    Added routing.

--- src/DisplayAccounts.js
+++ src/DisplayAccounts.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import './DisplayAccounts';
+import React, {useEffect, useState} from 'react'
 
-function displayAccounts() {
-    return (
-        <div>
-            Accounts:
-
-        </div>
-    );
+function useGetAccounts() {
+    // sets up local state inside function
+    // accounts = variable that stores state
+    // setAccountsData function that updates the Accounts variable
+    const [accounts, setAccountsData] = useState([])
+    useEffect(()=>{
+        // Makes request to URL:
+        fetch('http://localhost:5000/SQbank')
+            // formats the Json
+            .then((data)=>{return data.json()})
+            // .then (above) followed by a return passes the data down to the next .then as a parameter (below)
+            .then((data)=>{
+                // data is the array of objects in Json
+                setAccountsData(data)
+            })
+    }, accounts)
+    return accounts
 }
 
-export default displayAccounts;
+function DisplayAccounts() {
+    const accounts = useGetAccounts()
+    const accountElements = accounts.map((account) =>
+        <p>{account.name}: {account.balance}</p>
+    );
+
+    return (
+        <div className="">
+            <h1> { accountElements }</h1>
+        </div>
+    )
+}
+
+export default DisplayAccounts
--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'
 
-function useGetAccounts() {
-    // sets up local state inside function
-    // accounts = variable that stores state
-    // setAccountsData function that updates the Accounts variable
-    const [accounts, setAccountsData] = useState([])
-    useEffect(()=>{
-        // Makes request to URL:
-        fetch('http://localhost:5000/SQbank')
-            // formats the Json
-            .then((data)=>{return data.json()})
-            // .then (above) followed by a return passes the data down to the next .then as a parameter (below)
-            .then((data)=>{
-                // data is the array of objects in Json
-                setAccountsData(data)
-            })
-    }, accounts)
-    return accounts
+// Switch looks for 'true', if it doesn't find anything, then it will get to 'Not Found'
+// 'exact' looks for an exact regex match
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import DisplayAccounts from './DisplayAccounts';
+
+class Routing extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/accounts">View Accounts</Link>
+                        </li>
+                    </ul>
+                    <Switch>
+                        <Route exact path="/" component={App}/>
+                        <Route path="/accounts" component={DisplayAccounts}/>
+                    </Switch>
+                </div>
+            </Router>
+        )
+    }
 }
 
-function Accounts() {
-    const accounts = useGetAccounts()
-    const accountElements = accounts.map((account) =>
-        <p>{account.name}: {account.balance}</p>
-    );
-
-
-    return (
-        <div className="App">
-            <h1> { accountElements }</h1>
-        </div>
-    )
-}
-
-
-ReactDOM.render(<Accounts />, document.getElementById('root'));
+ReactDOM.render(<Routing />, document.getElementById('root'));
 
 
